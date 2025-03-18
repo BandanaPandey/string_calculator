a0e825d49f9209000e0e46843281c9a96a4b5152
BandanaPandey <bandanapandey11@gmail.com>
2025-03-18 21:43:24 +0530

    TDD: Support different delimiters Added testcase for supporting custom delimiters Added code to support this test-case Also, added code level documentation in this commit

--- app/services/string_calculator_service.rb
+++ app/services/string_calculator_service.rb
@@ -1,7 +1,24 @@
+# The StringCalculatorService class provides a method to add numbers from a string input.
+# It supports custom delimiters and handles empty input strings.
 class StringCalculatorService
+  # Adds the numbers in the given string.
+  #
+  # @param numbers [String] the string containing numbers to add, separated by delimiters.
+  # @return [Integer] the sum of the numbers in the string. Returns 0 if the string is empty.
   def add(numbers)
     return 0 if numbers.strip.empty?
 
-    numbers.split(',').map(&:to_i).sum
+    numbers.split(delimiter(numbers)).map(&:to_i).sum
+  end
+
+  private
+
+  # Determines the delimiter used in the input string.
+  #
+  # @param input [String] the input string containing numbers and possibly a custom delimiter.
+  # @return [Regexp, String] the delimiter to use for splitting the input string.
+  def delimiter(input)
+    return input[2] if input.match?("//(.*)\n(.*)")
+    %r([,\n])
   end
 end
--- spec/services/string_calculator_service_spec.rb
+++ spec/services/string_calculator_service_spec.rb
@@ -11,14 +11,38 @@ RSpec.describe StringCalculatorService, type: :service do
     end
 
     context 'when input is a single number' do
-      it 'returns the number itself' do
+      it 'returns the number itself for input 18' do
         expect(string_calculator.add("18")).to eq(18)
+      end
+
+      it 'returns the number itself for input 1' do
+        expect(string_calculator.add("1")).to eq(1)
       end
     end
 
     context 'when input is multiple numbers' do
-      it 'returns the sum of the numbers' do
+      it 'returns the sum of the numbers for input:(18,3,2025)' do
         expect(string_calculator.add("18,3,2025")).to eq(2046)
+      end
+
+      it 'returns the sum of the numbers for input:(1,2,3)' do
+        expect(string_calculator.add("1,2,3")).to eq(6)
+      end
+    end
+
+    context 'when input contains new lines between numbers' do
+      it 'returns the sum of the numbers' do
+        expect(string_calculator.add("1\n2,3")).to eq(6)
+      end
+    end
+
+    context 'when input contains custom delimiter' do
+      it 'returns the sum of the numbers when delimiter is ;' do
+        expect(string_calculator.add("//;\n1;2")).to eq(3)
+      end
+
+      it 'returns the sum of the numbers when delimiter is +' do
+        expect(string_calculator.add("//+\n1+2")).to eq(3)
       end
     end
   end
